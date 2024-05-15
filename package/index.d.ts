43982c12a9ccf0ab252a219d371538be6ef0d2ea
李健坤 <2024@shinow.com.cn>
2024-05-15 08:32:53 +0800

    feat: 增加类型声明

--- package/index.d.ts
+++ package/index.d.ts
@@ -1,1 +1,4 @@
-export function setupCounter(element: HTMLButtonElement): void
+export function HexToRgb(color: string): string
+export function RgbToHex(a: number, b: number, c: number): string
+export function getDarkColor(color: string, level: number): string
+export function getLightColor(color: string, level: number): string
