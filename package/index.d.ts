c667c46dbac9bb10b7960dcdead9bb191cc83d26
李健坤 <2024@shinow.com.cn>
2024-05-15 10:21:21 +0800

    feat: 修改类型声明

--- package/index.d.ts
+++
@@ -1,4 +0,0 @@
-export function HexToRgb(color: string): string
-export function RgbToHex(a: number, b: number, c: number): string
-export function getDarkColor(color: string, level: number): string
-export function getLightColor(color: string, level: number): string
--- package/vite.config.ts
+++ package/vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       fileName: 'index'
     }
   },
-  plugins: [dts({ rollupTypes: true })]
+  plugins: [dts()]
 })
